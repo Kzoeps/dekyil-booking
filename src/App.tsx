ddb150d1c021bfb76016172dc926c843a13781a4
kzoeps <kzoepa@gmail.com>
2022-09-17 16:18:01 -0400

    feat(stuff): done with some shit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React, {useState} from 'react';
-import reactLogo from './assets/react.svg';
 import './App.css';
 import {Field, Form, Formik} from 'formik';
 import {Select, TextField} from 'formik-mui';
 import {AdapterDayjs} from '@mui/x-date-pickers/AdapterDayjs';
-import {Button, MenuItem, TextField as MuiTextField} from '@mui/material';
+import {Button, MenuItem, Stack, TextField as MuiTextField} from '@mui/material';
 import {DatePicker, LocalizationProvider} from '@mui/x-date-pickers';
 import dayjs, {Dayjs} from 'dayjs';
 
@@ -33,56 +32,37 @@ function App() {
 
 	return (
 		<LocalizationProvider dateAdapter={AdapterDayjs}>
-			<div className="App">
-				<div>
-					<a href="https://vitejs.dev" target="_blank">
-						<img src="/vite.svg" className="logo" alt="Vite logo"/>
-					</a>
-					<a href="https://reactjs.org" target="_blank">
-						<img src={reactLogo} className="logo react" alt="React logo"/>
-					</a>
-				</div>
-				<h1>Vite + React</h1>
-				<div>
-					<Formik initialValues={initValues} onSubmit={(values) => {
-						console.log(convDateToReadable(values.date as unknown as Dayjs));
-					}}>
-						{(formik) => (
-							<Form>
-								<Field label={'First Name'} name={'firstName'} component={TextField}/>
-								<Field label={'Last Name'} name={'lastName'} component={TextField}/>
-								<Field label={'Number of rooms'} name={'rooms'} component={Select}>
-									{getRoomSelects()}
-								</Field>
+			<div style={{width: '100%', height: '80px',  boxShadow: '0 2px 25px 2px gray'}}>
+				Dekyil Guest House
+			</div>
+			<Formik initialValues={initValues} onSubmit={(values) => {
+				console.log(convDateToReadable(values.date as unknown as Dayjs));
+			}}>
+				{(formik) => (
+					<Form>
+						<Stack spacing={2} padding={5} mt={10}>
+							<Field label={'First Name'} name={'firstName'} component={TextField}/>
+							<Field label={'Last Name'} name={'lastName'} component={TextField}/>
+							<Field label={'Number of rooms'} name={'rooms'} component={Select}>
+								{getRoomSelects()}
+							</Field>
 
-								<DatePicker
-									label="Reservation Date"
-									inputFormat={'DD/MM/YYYY'}
-									value={formik.values.date}
-									onChange={(newValue) => {
-										formik.setFieldValue('date', newValue);
-									}}
-									renderInput={(params) => <MuiTextField {...params} />}
-								/>
-								<Button type="submit">
-									Submit
-								</Button>
+							<DatePicker
+								label="Reservation Date"
+								inputFormat={'DD/MM/YYYY'}
+								value={formik.values.date}
+								onChange={(newValue) => {
+									formik.setFieldValue('date', newValue);
+								}}
+								renderInput={(params) => <MuiTextField {...params} />}
+							/>
+							<Button variant={'contained'} type="submit">
+								Submit
+							</Button>
+						</Stack>
 							</Form>
 						)}
 					</Formik>
-				</div>
-				<div className="card">
-					<button onClick={() => setCount((count) => count + 1)}>
-						count is {count}
-					</button>
-					<p>
-						Edit <code>src/App.tsx</code> and save to test HMR
-					</p>
-				</div>
-				<p className="read-the-docs">
-					Click on the Vite and React logos to learn more
-				</p>
-			</div>
 		</LocalizationProvider>
 	);
 }
